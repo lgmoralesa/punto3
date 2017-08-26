5dd712442b9045322b9d765da571566bfbc8a0da
Laura Morales <lgmoralesa@unal.edu.co>
2017-08-26 04:58:13 +0000

    Cuarto commit, modelo 4 courses manual

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826045153) do
+ActiveRecord::Schema.define(version: 20170826045642) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.integer "code"
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
