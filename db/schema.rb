1148454f5ac33da4693e7f1c32e940a253e51836
Laura Morales <lgmoralesa@unal.edu.co>
2017-08-26 04:12:33 +0000

    segundo commit, modelo 2 admin manual

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826032740) do
+ActiveRecord::Schema.define(version: 20170826034410) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "name"
